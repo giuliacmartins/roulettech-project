8b6f0e738c2aab6bc8ca7399b0bab839b5d7d0e8
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-07-14 23:58:17 -0400

    django set up running

--- blog-backend/blog_project/blog/models.py
+++ blog-backend/blog_project/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# class Post(models.Model):
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+#     author = models.CharField(max_length=50)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
--- blog-backend/blog_project/blog/views.py
+++ blog-backend/blog_project/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from rest_framework import generics
+# from .models import Post, Comment
+# from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+# class PostListCreate(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostDetail(generics.RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class CommentCreate(generics.CreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+# blog/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, World! This is your Django app.")
