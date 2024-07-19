ec8236e9ac1126ce30508b0b0ff4846536796123
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-07-19 00:02:30 -0400

    fixed url pattern

--- blog-backend/blog_backend/api/urls.py
+++ blog-backend/blog_backend/api/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostsViewSet, UserViewSet
@@ -7,6 +8,10 @@ router = DefaultRouter()
 router.register('posts', PostsViewSet, basename='posts')
 router.register('users', UserViewSet)
 
+def redirect_to_api(request):
+    return redirect('/api/')
+
 urlpatterns = [
+    path('', redirect_to_api),
     path('api/', include(router.urls)),
 ]
